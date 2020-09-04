7a5a2de3664da2dc5bb89b5c082d4293e69c69bd
dmihi-tmihi <hidededededem@gmail.com>
2020-09-04 19:38:15 +0900

    DBへの保存を可能とする記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 
@@ -11,6 +12,9 @@ class ApplicationController < ActionController::Base
 
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name][:last_name][:first_name][:last_name_kana][:first_name_kana][:date_of_birth])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
