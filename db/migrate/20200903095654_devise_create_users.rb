3c292f5a57e33490cb3bb10969a94f61504d8b5b
dmihi-tmihi <hidededededem@gmail.com>
2020-09-04 18:12:34 +0900

    マイグレーションファイル修正

--- db/migrate/20200903095654_devise_create_users.rb
+++ db/migrate/20200903095654_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name,         null: false, default: ""
       t.string :last_name_kana,     null: false, default: ""
       t.string :first_name_kana,    null: false, default: ""
-      t.date   :date_of_birth,      null: false, default: ""
+      t.date   :date_of_birth,      null: false, default: 0
 
       ## Recoverable
       t.string   :reset_password_token
