fd077697b1af0606f58707871afe97710c8b6859
dmihi-tmihi <hidededededem@gmail.com>
2020-09-05 07:22:33 +0900

    データベースへの保存を可能とする記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name][:last_name][:first_name][:last_name_kana][:first_name_kana][:date_of_birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name , :last_name , :first_name , :last_name_kana , :first_name_kana , :date_of_birth])
   end
 
   def basic_auth
