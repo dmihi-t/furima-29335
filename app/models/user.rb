631c7adab8154ff9629c24f999958cf3c5e05578
dmihi-tmihi <hidededededem@gmail.com>
2020-09-05 10:09:59 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true , uniqueness: false , format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :password, presence: true , length: {minimum: 6} , format: {with: /[a-z]{1,}[\d]{1,}/}
+  validates :password_confirmation, presence: true
+  validates :date_of_birth, presence: true
+
+  with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+  end
+  with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+    validates :first_name_kana, presence: true
+    validates :last_name_kana, presence: true
+  end
 end
