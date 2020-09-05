e83b18fca8d5d3af73b4e257f7a0a649ab07c504
dmihi-tmihi <hidededededem@gmail.com>
2020-09-05 16:24:06 +0900

    factorybotファイル修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,12 @@ FactoryBot.define do
   factory :user do
     name                  {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"aaa111"}
     password_confirmation {password}
+    date_of_birth         {"1983-09-08"}
+    first_name            {"山本"}
+    last_name             {"工事"}
+    first_name_kana       {"ヤマモト"}
+    last_name_kana        {"コウジ"}
   end
 end
