ce075cf846fb34e6146dcbbe79f3516537ad71cf
dmihi-tmihi <hidededededem@gmail.com>
2020-09-06 21:20:58 +0900

    new＆createアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,30 @@ class ItemsController < ApplicationController
 
   def index
   end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
   end
+
+  def item_params
+    params.require(:item).permit(:name,:description,:category_id,:status_id,:delivery_fee_id,:prefecture_id,:days_shipping_id,:price)
+  end
+  
 end
