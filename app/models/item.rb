e60280850a90ad4664d9d5a4fec85fb837c66e53
dmihi-tmihi <hidededededem@gmail.com>
2020-09-07 13:15:29 +0900

    priceのvalidates記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,6 @@ class Item < ApplicationRecord
 
   validates :category_id, :status_id, :delivery_fee_id, :prefecture_id, :days_shipping_id, numericality: { other_than: 1 } 
 
+  validates :price, format: {with: /\A[0-9]+\z/}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
