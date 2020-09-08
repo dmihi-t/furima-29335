1ebc992f243e3a29eee1587deb173addd0c5e4fa
dmihi-tmihi <hidededededem@gmail.com>
2020-09-09 07:09:06 +0900

    小数点以下切り捨て記述追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,7 +21,7 @@ window.addEventListener('load', function(){
   const price2 = document.getElementById("add-tax-price")
   const price3 = document.getElementById("profit")
   price1.addEventListener('input', function() {
-  price2.innerHTML = (price1.value) * 0.1
-  price3.innerHTML = (price1.value) * 0.9
+  price2.innerHTML = Math.floor((price1.value) * 0.1)
+  price3.innerHTML = Math.floor((price1.value) * 0.9)
   })
 })
