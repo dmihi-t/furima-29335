2364c1bf522b1815f88e09c3c990187173dbefeb
dmihi-tmihi <hidededededem@gmail.com>
2020-09-10 05:27:33 +0900

    もとに戻しました。

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,11 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(){
-  window.addEventListener('turbolinks:load', function(){
-    const price = document.getElementById("item-price")
-    const add_tax_price = document.getElementById("add-tax-price")
-    const profit = document.getElementById("profit")
-    price.addEventListener('input', function() {
-    add_tax_price.innerHTML = Math.floor((price.value) * 0.1)
-    profit.innerHTML = Math.floor((price.value) * 0.9)
-    })
+document.addEventListener("turbolinks:load", function() {
+  const price = document.getElementById("item-price")
+  const add_tax_price = document.getElementById("add-tax-price")
+  const profit = document.getElementById("profit")
+  price.addEventListener('input', function() {
+  add_tax_price.innerHTML = Math.floor((price.value) * 0.1)
+  profit.innerHTML = Math.floor((price.value) * 0.9)
   })
 })
