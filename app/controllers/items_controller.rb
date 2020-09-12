d2affa813bd1ec4e8fca6e467a9f0d4984b69e54
dmihi-tmihi <hidededededem@gmail.com>
2020-09-12 17:26:48 +0900

    不要なsaveメソッド削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,8 +31,7 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
+    if @item.update(item_params)
       redirect_to item_path(item_params)
     else
       render :edit
