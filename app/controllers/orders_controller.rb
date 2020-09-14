52b27e55e9c8cd6723bffe39f4c8a76560c06f28
dmihi-tmihi <hidededededem@gmail.com>
2020-09-15 07:10:07 +0900

    redirect_to設定
    出品者が直接その購入ページへ遷移しようとするとトップページへ遷移する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,7 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     # @order = @item.order
+    redirect_to root_path if current_user.id == @item.user_id
   end
   
   def create
