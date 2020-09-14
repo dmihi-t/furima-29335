28087a9ce6c279e9adf0b80757c9fb0f8d18f84c
dmihi-tmihi <hidededededem@gmail.com>
2020-09-14 09:09:04 +0900

    ShippingAddressモデルにvalidates記述

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,4 +2,9 @@ class ShippingAddress < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :order
   belongs_to_active_hash :prefecture
+
+  validates :postal_code, :city, :address, :phone_number, presence: true
+  validates :postal_code, format: { with: /^\d{3}[-]\d{4}$/ }
+  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :phone_number, length: { maximum: 11 }
 end
