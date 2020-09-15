87e9362d1ffc1959d459bdeca5aaaba4ada28691
dmihi-tmihi <hidededededem@gmail.com>
2020-09-15 09:24:52 +0900

    呼び出しメソッド記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,6 @@ class OrdersController < ApplicationController
   
   def index
     @item = Item.find(params[:item_id])
-    # @order = @item.order
     redirect_to root_path if current_user.id == @item.user_id
   end
   
