bfc4ec3039af1ee063fbe963db68fae8e9782c13
dmihi-tmihi <hidededededem@gmail.com>
2020-09-15 13:58:43 +0900

    マイグレーションファイル修正

---
+++ app/forms/order_shipping.rb
@@ -0,0 +1,16 @@
+class OrderShipping
+
+  include ActiveModel::Model
+  attr_accessor :token, :postal_code, :prefectures_id, :city, :address, :phone_number
+
+  validates :token, :postal_code, :city, :address, :phone_number, presence: true
+  validates :postal_code, format: { with: /^\d{3}[-]\d{4}$/ }
+  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :phone_number, length: { maximum: 11 }
+
+  def save
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture.id, city: city, address: address, phone_number: phone_number,order_id: order.id)
+    Order.create(item_id: @item.id, user_id: @item.user.id, )
+  end
+  
+end
