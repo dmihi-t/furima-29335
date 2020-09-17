c4e07f671bc426b4f5d537380ecff84bf17a55d4
dmihi-tmihi <hidededededem@gmail.com>
2020-09-17 20:34:16 +0900

    phone_numberのvalidates追加記述

--- app/forms/order_shipping.rb
+++ app/forms/order_shipping.rb
@@ -6,7 +6,7 @@ class OrderShipping
   validates :token, :postal_code, :city, :address, :phone_number, presence: true
   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: { other_than: 0 }
-  validates :phone_number, length: { maximum: 11 }
+  validates :phone_number, length: { maximum: 11 }, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
