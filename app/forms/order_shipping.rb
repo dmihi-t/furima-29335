b0054b5b4e637f1ef2036935f17aa1b79100a9fc
dmihi-tmihi <hidededededem@gmail.com>
2020-09-17 18:43:33 +0900

    formオブジェクト制作

--- app/forms/order_shipping.rb
+++ app/forms/order_shipping.rb
@@ -1,16 +1,16 @@
 class OrderShipping
 
   include ActiveModel::Model
-  attr_accessor :token, :postal_code, :prefectures_id, :city, :address, :phone_number
+  attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :phone_number, :building_name, :user_id, :item_id
 
   validates :token, :postal_code, :city, :address, :phone_number, presence: true
-  validates :postal_code, format: { with: /^\d{3}[-]\d{4}$/ }
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: { other_than: 0 }
   validates :phone_number, length: { maximum: 11 }
 
   def save
-    Order.create(item_id: @item.id, user_id: @item.user.id, )
-    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture.id, city: city, address: address, building_name: building_name, phone_number: phone_number,order_id: order.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number,order_id: order.id)
   end
   
 end
